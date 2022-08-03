320da08cc45fe872bbcd741945d20fa1404b8400
martynov-94 <martynovsg94@gmail.com>
2022-08-03 11:36:19 +0300

    header-catalog desktop versions

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const menuBtnIcon = document.querySelector(".menu-btn");
 const description = document.querySelector(".description");
 const descriptionBackdrop = document.querySelector(".description__backdrop");
 
+console.log(description.offsetHeight);
 descriptionBackdrop.style.height = description.offsetHeight + "px";
 
 menuBtn.onclick = () => {
   const classToAdd = "main-menu_open";
+
   menu.classList.toggle(classToAdd);
   menu.style.transform = `scaleY(${+menu.classList.contains(classToAdd)})`;
 };
 
+
 window.onresize = () => descriptionBackdrop.style.height = description.offsetHeight + "px";
 
-console.log(window.innerWidth);
